efedb2c8de0b27e0cc4994e29d1e92032b7b65ed
ken yasue <ken@yasue.us>
2021-09-30 14:45:32 +0200

    add authtag to file head

--- README.md
+++ README.md
@@ -2,16 +2,28 @@
 
 ### How to use
 
+#### install
+
+```
+git clone https://github.com/kenyasue/learning_encryption.git
+cd learning_encryption.git
+npm install
+```
+
 #### Encrypt
 
 ```
+
 npm run encrypt -- -i [input file path] -p [password]
+
 ```
 
 In this case encrypted file is generated under same dir as input file with ".enc" suffix.
 
 ```
-npm run encrypt -- -i [input file path] -o  [output file path] -p [password]
+
+npm run encrypt -- -i [input file path] -o [output file path] -p [password]
+
 ```
 
 You can specify output file path.
@@ -19,13 +31,27 @@ You can specify output file path.
 #### Decrypt
 
 ```
+
 npm run decrypt -- -i [input file path] -p [password]
+
 ```
 
 In this case decrypted file is generated under same dir as input file with ".enc" suffix.
 
 ```
-npm run decrypt -- -i [input file path] -o  [output file path] -p [password]
+
+npm run decrypt -- -i [input file path] -o [output file path] -p [password]
+
 ```
 
 You can specify output file path.
+
+#### File Structure
+
+| 16bytes of authtag | 16bytes of initial vector | encrypted binary |
+
+#### Refecence
+
+https://medium.com/@brandonstilson/lets-encrypt-files-with-node-85037bea8c0e
+
+https://gist.github.com/rjz/15baffeab434b8125ca4d783f4116d81
--- src/SimpleCrypt.ts
+++ src/SimpleCrypt.ts
@@ -2,6 +2,9 @@ import * as crypto from "crypto";
 import * as fs from "fs";
 import { Transform } from "stream";
 
+const INITVECTOR_SIZE: number = 16;
+const AUTHTAG_SIZE: number = 16;
+
 export class AppendInitVect extends Transform {
   initVect: Buffer;
   appended: boolean;
@@ -22,8 +25,28 @@ export class AppendInitVect extends Transform {
   }
 }
 
+export class AppendAuthTag extends Transform {
+  authTag: Buffer;
+  appended: boolean;
+
+  constructor(authTag: Buffer, opts = undefined) {
+    super(opts);
+    this.authTag = authTag;
+    this.appended = false;
+  }
+
+  _transform(chunk: Buffer, encoding: string, cb: Function) {
+    if (!this.appended) {
+      this.push(this.authTag);
+      this.appended = true;
+    }
+    this.push(chunk);
+    cb();
+  }
+}
+
 export default class SimpleCrypt extends Transform {
-  algorithm: string = "aes-256-gcm";
+  algorithm: crypto.CipherGCMTypes = "aes-256-gcm";
 
   getCipherKey(password): Buffer {
     return crypto.createHash("sha256").update(password).digest();
@@ -48,12 +71,14 @@ export default class SimpleCrypt extends Transform {
     return new Promise((res, rej) => {
       // start encrypting
       const readStream: fs.ReadStream = fs.createReadStream(inputFile);
-      const writeStream: fs.WriteStream = fs.createWriteStream(outputFile);
+      const writeStream: fs.WriteStream = fs.createWriteStream(
+        outputFile + ".tmp"
+      );
 
-      const initVect: Buffer = crypto.randomBytes(16);
+      const initVect: Buffer = crypto.randomBytes(INITVECTOR_SIZE);
 
       const cipherKey = this.getCipherKey(password);
-      const cipher: crypto.Cipher = crypto.createCipheriv(
+      const cipher: crypto.CipherGCM = crypto.createCipheriv(
         this.algorithm,
         cipherKey,
         initVect
@@ -63,7 +88,28 @@ export default class SimpleCrypt extends Transform {
       readStream.pipe(cipher).pipe(appendInitVect).pipe(writeStream);
 
       writeStream.on("close", () => {
-        res();
+        const authTag: Buffer = cipher.getAuthTag();
+
+        // append authtag to file
+        const appendAuthTag = new AppendAuthTag(authTag);
+
+        const readStreamAuthTag: fs.ReadStream = fs.createReadStream(
+          outputFile + ".tmp"
+        );
+        const writeStreamAuthTag: fs.WriteStream =
+          fs.createWriteStream(outputFile);
+
+        readStreamAuthTag.pipe(appendAuthTag).pipe(writeStreamAuthTag);
+
+        readStreamAuthTag.on("close", () => {
+          fs.unlinkSync(outputFile + ".tmp");
+          res();
+        });
+
+        readStreamAuthTag.on("error", (e) => {
+          fs.unlinkSync(outputFile + ".tmp");
+          rej(e);
+        });
       });
 
       readStream.on("error", (e) => {
@@ -74,7 +120,10 @@ export default class SimpleCrypt extends Transform {
 
   readIVFromFile(inputFile): Promise<Buffer> {
     return new Promise((res, rej) => {
-      const readIV = fs.createReadStream(inputFile, { end: 15 });
+      const readIV = fs.createReadStream(inputFile, {
+        start: AUTHTAG_SIZE,
+        end: INITVECTOR_SIZE + AUTHTAG_SIZE - 1,
+      });
       let initVect: Buffer | null = null;
       readIV.on("data", (chunk: Buffer) => {
         initVect = chunk;
@@ -83,37 +132,58 @@ export default class SimpleCrypt extends Transform {
         res(initVect);
       });
       readIV.on("error", () => {
+        console.log("error");
         rej();
       });
     });
   }
 
-  decryptFile(inputFile, outputFile, password): Promise<void> {
-    return this.readIVFromFile(inputFile).then((initVect) => {
-      return new Promise((res, rej) => {
-        // start decrypting
-        const readStream: fs.ReadStream = fs.createReadStream(inputFile, {
-          start: 16,
-        });
-        const writeStream: fs.WriteStream = fs.createWriteStream(outputFile);
+  readAuthTagFile(inputFile): Promise<Buffer> {
+    return new Promise((res, rej) => {
+      const readIV = fs.createReadStream(inputFile, {
+        start: 0,
+        end: AUTHTAG_SIZE - 1,
+      });
+      let authTag: Buffer | null = null;
+      readIV.on("data", (chunk: Buffer) => {
+        authTag = chunk;
+      });
+      readIV.on("close", () => {
+        res(authTag);
+      });
+      readIV.on("error", () => {
+        rej();
+      });
+    });
+  }
 
-        const cipherKey = this.getCipherKey(password);
-        const cipher: crypto.Cipher = crypto.createDecipheriv(
-          this.algorithm,
-          cipherKey,
-          initVect
-        );
+  async decryptFile(inputFile, outputFile, password): Promise<void> {
+    const authTag: Buffer = await this.readAuthTagFile(inputFile);
+    const initVect: Buffer = await this.readIVFromFile(inputFile);
 
-        const appendInitVect = new AppendInitVect(initVect);
-        readStream.pipe(cipher).pipe(writeStream);
+    return await new Promise((res, rej) => {
+      const readStream: fs.ReadStream = fs.createReadStream(inputFile, {
+        start: INITVECTOR_SIZE + AUTHTAG_SIZE,
+      });
+      const writeStream: fs.WriteStream = fs.createWriteStream(outputFile);
 
-        writeStream.on("close", () => {
-          res();
-        });
+      const cipherKey = this.getCipherKey(password);
+      const cipher: crypto.DecipherGCM = crypto.createDecipheriv(
+        this.algorithm,
+        cipherKey,
+        initVect
+      );
+      cipher.setAuthTag(authTag);
 
-        readStream.on("error", (e) => {
-          rej(e);
-        });
+      const appendInitVect = new AppendInitVect(initVect);
+      readStream.pipe(cipher).pipe(writeStream);
+
+      writeStream.on("close", () => {
+        res();
+      });
+
+      readStream.on("error", (e) => {
+        rej(e);
       });
     });
   }
